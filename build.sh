f532e66dfc376d8543e9b3b7e884e265414bb1d8
linckandrea <linckandrea01@gmail.com>
2024-08-12 16:27:47 +0200

    InitialSetup

---
+++ build.sh
@@ -0,0 +1,8 @@
+mkdir -p out
+export ARCH=arm
+export SUBARCH=arm
+make O=out clean
+make O=out mrproper
+export CROSS_COMPILE=$HOME/Android-dev/toolchains/aosp-clang/arm-linux-androideabi-4.9/bin/arm-linux-androidkernel-
+make O=out sawshark_defconfig 
+make O=out -j$(nproc --all) 
